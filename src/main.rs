9fc772c0d21cbc1a044fa03251396d448f7c6992
Ben Booth <benwbooth@gmail.com>
2022-02-15 19:47:05 +0000

    change args

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,25 @@ use sha2::Digest;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: {} input.fastq to_check.fastq > missing_reads.fastq", args[0]);
+    if args.len() < 2 {
+        eprintln!("Usage: {} to_check.fastq ... < input.fastq > missing_reads.fastq", args[0]);
         std::process::exit(1);
     }
     let mut record = fastq::Record::new();
     let mut hashset = HashSet::new();
-    let mut reader = fastq::Reader::from_file(&args[1])?;
-    let mut reader2 = fastq::Reader::from_file(&args[2])?;
+    let mut reader2 = fastq::Reader::new(io::stdin());
     let mut writer = fastq::Writer::new(io::stdout());
     let mut total = 0u64;
     let mut notfound = 0u64;
 
-    reader.read(&mut record)?;
-    while !record.is_empty() {
-        let hash = Sha256::new().chain_update(record.seq()).finalize();
-        hashset.insert(hash);
+    for arg in &args[1..args.len()] {
+        let mut reader = fastq::Reader::from_file(&arg)?;
         reader.read(&mut record)?;
+        while !record.is_empty() {
+            let hash = Sha256::new().chain_update(record.seq()).finalize();
+            hashset.insert(hash);
+            reader.read(&mut record)?;
+        }
     }
     let mut record = fastq::Record::new();
     reader2.read(&mut record)?;
