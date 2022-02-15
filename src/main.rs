0f21a24fb73a659a5ae87849556f6a8fe5e7a397
Ben Booth <benwbooth@gmail.com>
2022-02-15 19:17:49 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::env;
+use std::io;
+use std::collections::HashSet;
+use sha2::Sha256;
+use bio::io::fastq;
+use bio::io::fastq::FastqRead;
+use anyhow::Result;
+use sha2::Digest;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} input.fastq to_check.fastq > missing_reads.fastq", args[0]);
+        std::process::exit(1);
+    }
+    let mut record = fastq::Record::new();
+    let mut hashset = HashSet::new();
+    let mut reader = fastq::Reader::from_file(&args[1])?;
+    let mut reader2 = fastq::Reader::from_file(&args[2])?;
+    let mut writer = fastq::Writer::new(io::stdout());
+
+    reader.read(&mut record)?;
+    while !record.is_empty() {
+        let hash = Sha256::new().chain_update(record.seq()).finalize();
+        hashset.insert(hash);
+        reader.read(&mut record)?;
+    }
+    reader2.read(&mut record)?;
+    while !record.is_empty() {
+        let hash = Sha256::new().chain_update(record.seq()).finalize();
+        if !hashset.contains(&hash) {
+            writer.write_record(&record)?;
+        }
+        reader2.read(&mut record)?;
+    }
+    Ok(())
+}
