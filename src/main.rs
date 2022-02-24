81d04d5680d871d0b865c485fa02b8b63d5874ea
Ben Booth <benwbooth@gmail.com>
2022-02-24 22:28:44 +0000

    lots of fixes, handle fastq.gz

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,94 @@
 use std::env;
-use std::io;
+use std::collections::HashMap;
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::io::Read;
+use std::io::Write;
 use sha2::Sha256;
+use sha2::digest::Digest;
 use bio::io::fastq;
 use bio::io::fastq::FastqRead;
 use anyhow::Result;
-use sha2::Digest;
+use flate2::read::GzDecoder;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} query.fastq ... < to_index.fastq > missing_reads.fastq", args[0]);
+    if args.len() < 3 {
+        eprintln!("Usage: {} to_index_fastq_files.list query_fastq_files.list > missing_reads.fastq", args[0]);
         std::process::exit(1);
     }
-    let mut hashset = HashSet::new();
-    let mut reader = fastq::Reader::new(io::stdin());
-    let mut record = fastq::Record::new();
-    reader.read(&mut record)?;
-    while !record.is_empty() {
-        let hash = Sha256::new().chain_update(record.seq()).finalize();
-        hashset.insert(hash);
-        reader.read(&mut record)?;
+
+    let to_index_fastq_files = File::open(&args[1])?;
+    let mut to_index_fastq_files_br = BufReader::new(to_index_fastq_files);
+    let mut line = String::new();
+    let mut to_index_fastq = Vec::<String>::new();
+    while to_index_fastq_files_br.read_line(&mut line)? > 0 {
+        to_index_fastq.push(line.trim_end_matches(['\r','\n']).to_string());
     }
 
-    let mut total = 0u64;
-    let mut notfound = 0u64;
-    let mut writer = fastq::Writer::new(io::stdout());
-    for arg in &args[1..args.len()] {
+    let mut seed = Sha256::new();
+    let mut bytes = [0u8; 32];
+    let mut hashmap = HashMap::<[u8; 32], Vec<usize>>::new();
+    for (i, to_index) in to_index_fastq.iter().enumerate() {
+        let mut reader: fastq::Reader<BufReader<Box<dyn Read>>> = if to_index.ends_with(".gz") {
+            fastq::Reader::new(Box::new(GzDecoder::new(File::open(to_index)?)))
+        } else {
+            fastq::Reader::new(Box::new(File::open(to_index)?))
+        };
         let mut record = fastq::Record::new();
-        let mut reader2 = fastq::Reader::from_file(&arg)?;
-        reader2.read(&mut record)?;
+        reader.read(&mut record)?;
         while !record.is_empty() {
-            total += 1;
-            let hash = Sha256::new().chain_update(record.seq()).finalize();
-            if !hashset.contains(&hash) {
-                writer.write_record(&record)?;
-                notfound += 1;
+            seed.reset();
+            seed.update(record.seq());
+            seed.write(&mut bytes)?;
+            match hashmap.get_mut(&bytes[..]) {
+                Some(vec) => vec.push(i),
+                None => {
+                    let mut vec = Vec::new();
+                    vec.push(i);
+                    hashmap.insert(bytes.clone(), vec);
+                }
             }
-            reader2.read(&mut record)?;
+            reader.read(&mut record)?;
         }
     }
-    eprintln!("Missing {} of {} reads", notfound, total);
+
+    let mut line = String::new();
+    let query_fastq_files = File::open(&args[1])?;
+    let mut query_fastq_files_br = BufReader::new(query_fastq_files);
+    while query_fastq_files_br.read_line(&mut line)? > 0 {
+        let file = line.trim_end_matches(['\r','\n']);
+        let mut reader: fastq::Reader<BufReader<Box<dyn Read>>> = if file.ends_with(".gz") {
+            fastq::Reader::new(Box::new(GzDecoder::new(File::open(file)?)))
+        } else {
+            fastq::Reader::new(Box::new(File::open(file)?))
+        };
+        let mut fileset = HashSet::<usize>::new();
+        let mut record = fastq::Record::new();
+        while reader.read(&mut record).is_err() { }
+        while !record.is_empty() {
+            seed.reset();
+            seed.update(record.seq());
+            seed.write(&mut bytes)?;
+            match hashmap.get(&bytes[..]) {
+                Some(vec) => {
+                    fileset.extend(vec);
+                },
+                None => {
+                    let id = record.id();
+                    eprintln!("{file}\t{id}\tmissing");
+                }
+            }
+            while reader.read(&mut record).is_err() { }
+        }
+        let mut found_in = Vec::new();
+        for i in fileset.iter() {
+            found_in.push(to_index_fastq[*i].clone());
+        }
+        let found_in_str = found_in.join(",");
+        println!("{file}\t{found_in_str}")
+    }
     Ok(())
 }
