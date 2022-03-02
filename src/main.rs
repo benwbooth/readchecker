5a9c669816e8e1a7825f18a6369297c1b5b744ec
Ben Booth <benwbooth@gmail.com>
2022-03-02 23:41:55 +0000

    fix sha256 hashing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::io::Read;
-use std::io::Write;
 use sha2::Sha256;
 use sha2::digest::Digest;
 use bio::io::fastq;
@@ -17,6 +16,9 @@ use rayon::prelude::*;
 use dashmap::DashMap;
 use atomic_counter::AtomicCounter;
 use atomic_counter::RelaxedCounter;
+use generic_array::GenericArray;
+use generic_array::typenum::U32;
+
 
 fn main() -> Result<()> {
     let threads = match std::env::var("THREADS") {
@@ -43,16 +45,15 @@ fn main() -> Result<()> {
         line.clear();
     }
 
-    let seqsha2fileidxset = DashMap::<[u8; 32], BTreeSet<u32>>::new();
+    let seqsha2fileidxset = DashMap::<GenericArray<u8, U32>, BTreeSet<u32>>::new();
     let counter = RelaxedCounter::new(0usize);
     let result: Result<(), anyhow::Error> = to_index_fastq.par_iter().enumerate().try_for_each(|(i, to_index)| {
-        let mut seed = Sha256::new();
-        let mut bytes = [0u8; 32];
         let mem_usage_bytes = procfs::process::Process::myself()?.stat()?.rss as u64 * bytes_per_page as u64;
         let c = counter.inc();
-        eprintln!("Indexing file {to_index} ({c}/{to_index_length}), memory usage={memusage}", 
+        eprintln!("Indexing file {to_index} ({c}/{to_index_length}), total reads={reads}, memory usage={memusage}", 
             c=c+1, 
             to_index_length=to_index_fastq.len(), 
+            reads=seqsha2fileidxset.len(),
             memusage=Byte::from_bytes(mem_usage_bytes as u128).get_appropriate_unit(false).to_string());
         let mut reader: fastq::Reader<BufReader<Box<dyn Read>>> = if to_index.ends_with(".gz") {
             fastq::Reader::new(Box::new(GzDecoder::new(File::open(to_index)?)))
@@ -62,9 +63,10 @@ fn main() -> Result<()> {
         let mut record = fastq::Record::new();
         while reader.read(&mut record).is_err() { }
         while !record.is_empty() {
-            seed.reset();
+            let mut seed = Sha256::new();
             seed.update(record.seq());
-            seed.write(&mut bytes)?;
+            let bytes  = seed.finalize();
+            //eprintln!("seq={seq}, bytes={bytes:?}", seq=std::str::from_utf8(record.seq())?);
             match seqsha2fileidxset.get_mut(&bytes) {
                 Some(mut fileidxset) => {
                     (*fileidxset).insert(i as u32);
@@ -73,7 +75,7 @@ fn main() -> Result<()> {
                 _ => {
                     let mut fileidxset = BTreeSet::new();
                     fileidxset.insert(i as u32);
-                    seqsha2fileidxset.insert(bytes.clone(), fileidxset);
+                    seqsha2fileidxset.insert(bytes, fileidxset);
                 }
             }
             while reader.read(&mut record).is_err() { }
@@ -99,8 +101,6 @@ fn main() -> Result<()> {
             c=c+1,
             query_fastq_length=query_fastq.len(), 
             memusage=Byte::from_bytes(mem_usage_bytes as u128).get_appropriate_unit(false).to_string());
-        let mut seed = Sha256::new();
-        let mut bytes = [0u8; 32];
         let mut reader: fastq::Reader<BufReader<Box<dyn Read>>> = if file.ends_with(".gz") {
             fastq::Reader::new(Box::new(GzDecoder::new(File::open(file)?)))
         } else {
@@ -110,9 +110,9 @@ fn main() -> Result<()> {
         let mut record = fastq::Record::new();
         while reader.read(&mut record).is_err() { }
         while !record.is_empty() {
-            seed.reset();
+            let mut seed = Sha256::new();
             seed.update(record.seq());
-            seed.write(&mut bytes)?;
+            let bytes  = seed.finalize();
             match seqsha2fileidxset.get(&bytes) {
                 Some(fileidxset) => {
                     match fileidxset2count.get_mut(&*fileidxset) {
